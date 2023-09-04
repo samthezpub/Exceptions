4a372be36526d9d27284b417fdc7479386465eb9
Vladimir <70057256+samthezpub@users.noreply.github.com>
2023-09-04 22:01:23 +0300

    ATM, Bank, User

--- src/atm/Main.java
+++ src/atm/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Bank aval = new Bank("Аваль", 0);
         ATM firstBankATM = new ATM(aval);
-        User Valera = new User(500);
+        User Valera = new User(500, 0);
         System.out.println(firstBankATM.getCurrentMoney());
         firstBankATM.depositBanknote(Banknotes.FIVEHUNDRED, Valera);
         System.out.println(firstBankATM.getCurrentMoney());
